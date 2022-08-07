6ccb0bee323b2d4cd0f37f552330ffac1a764851
Gonzalo Dos Santos <gonzalotomasdossantos@gmail.com>
2022-08-07 19:32:56 -0300

    Solucionados errores estéticos

--- player.js
+++ player.js
@@ -3,34 +3,35 @@ const text = document.getElementById('text')
 /* Setting the audio file and setting the loop */
 const audio = new Audio('bg-music.mp3')
 audio.loop = true;
-if(getCurrentTime()!=null){
-  audio.currentTime =getCurrentTime()
-}
 
-/* Do what the button says */
+if (localStorage.getItem('audioCurrentTime' === null)) {
+  localStorage.setItem('audioCurrentTime', '0')
+} else {
+  audio.currentTime = getCurrentTime();
+}
 
 setInterval(() => {
   setCurrentTime(audio.currentTime)
 }, 1000)
 
-if (localStorage.getItem('audioCurrentTime' === null)) {
-  localStorage.setItem('audioCurrentTime', '0')
-}
+
 
 function getCurrentTime(){
   return localStorage.getItem('audioCurrentTime')
 }
 
+
 function setCurrentTime(currentTime){
   localStorage.setItem('audioCurrentTime', currentTime)
 }
+/* Do what the button says */
 
 function playMusic(buttonText){
   if (buttonText === "Reproducir Música") {
     audio.currentTime = getCurrentTime()
     audio.play();
     btnMusic.innerText = 'Pausar Música'
-    text.innerText = 'Reproduciendo: ¿Qué paso con el fantasma del descenso? - PinoVersiona'
+    text.innerHTML = 'Reproduciendo: ¿Qué paso con el fantasma del descenso? - PinoVersiona'
   }
   if (buttonText === "Pausar Música") {
     audio.pause();
